87b3c07dd685345882869a0c9f62a86ae3c8841b
Tal Attias <tala585@gmail.com>
2024-01-30 22:03:07 +0200

    delete unnesicery keys from mongo

--- models/Product.js
+++ models/Product.js
@@ -3,6 +3,15 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
 	name: String,
 	price: Number,
+}, {
+	toJSON: {
+		virtuals: true,
+		versionKey: false,
+		transform: function (doc, ret) {
+			ret.id = ret._id; // Rename '_id' to 'id'
+			delete ret._id; // Remove the '_id' property
+		}
+	}
 });
 
 module.exports = mongoose.model('product', productSchema);
